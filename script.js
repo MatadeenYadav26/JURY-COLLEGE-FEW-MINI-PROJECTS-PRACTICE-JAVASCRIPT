d2a47e2898462a237616758c0a09066ce6d4e7e4
KING <matadeenyadav202@gmail.com>
2026-03-12 09:49:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-var bdy = document.querySelector('#bdy')
 var btn = document.querySelector('#btn')
-// var colors = ['red','blue','green']
-// var i = 0
+var table =document.querySelector('#table')
 
-// btn.addEventListener('click',function(){
-
-//     bdy.style.backgroundColor=colors[i]
-//     i++
-
-//     if(i>=colors.length){
-//         i=0
-//     }
-// })
-
+var colors = ['red','green','blue']
+var count = 0
 
 btn.addEventListener('click',function(){
-    var r = Math.floor(Math.random()*265)
-    var g = Math.floor(Math.random()*265)
-    var b = Math.floor(Math.random()*265)
-    var color = "rgb("+r+","+g+","+b+")"
 
-    bdy.style.backgroundColor=color;
+    var row =   document.createElement('tr')
+    var cell = document.createElement('td')
+
+    cell.innerHTML= "Row" + (count+1)
+    row.style.backgroundColor=colors[count%colors.length]
+
+    row.appendChild(cell)
+    table.appendChild(row)
+
+    count++
 })
